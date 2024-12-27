823ed7ee55f07f4da98f2ff3ffb0c8debc3ec685
Kalai selvi M <kalaijodhi9992@gmail.com>
2024-12-27 15:07:11 +0530

    TaskForm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import TaskForm from "./components/TaskForm";
+import './Style.css';
+import TaskList from "./components/TaskList";
+import ProgressTracker from "./components/ProgressTracker";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTask = (task)=>{
+    setTasks([...tasks,task])
+  };
+
+  const updateTask = (index, updatedTask) => {
+    const newTasks = [...tasks];
+    newTasks[index] = updatedTask;
+    setTasks(newTasks);
+  };
+
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
+  const clearTasks = () => {
+    setTasks([]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <header class="header">
+  <div class="header-container">
+    <h1 class="title">
+      Task<span class="highlight">Buddy</span>
+    </h1>
+    <p class="tagline">Your friendly task manager</p>
+  </div>
+</header>
+      <TaskForm addTask = {addTask} />
+      <TaskList
+        tasks={tasks}
+        updateTask={updateTask}
+        deleteTask={deleteTask}
+      />
+      <ProgressTracker tasks={tasks} />
+
+      {tasks.length>0 && (
+        <button className="clear-btn" onClick={clearTasks}>
+          Clear All Tasks
+        </button>
+       )}
+
     </div>
   );
 }
